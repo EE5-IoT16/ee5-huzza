d3276d680c5579103a9833d8aa529b2bd04506b5
roza <recepomer.zorlu@student.kuleuven.be>
2022-03-15 14:59:53 +0100

    Automatic database maintance took place. Credentials updated.

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgres://moyzrfdfaeifpv:bb7254ca8e867367ae8a6d8c41bc1ba2e4111140087a5de8140839f06ae2e990@ec2-52-208-185-143.eu-west-1.compute.amazonaws.com:5432/d7p2ahuv6uogme",
+  connectionString: "postgres://uykqvpkrwoxnnn:66bc248333e6bc87cbf78411954d89d5e156134f856cb599383234b448a29cb3@ec2-63-34-130-73.eu-west-1.compute.amazonaws.com:5432/d2b0d37k8s2rif",
   ssl: {
     rejectUnauthorized: false
   }
