c89fcea454823e1e3289ba55d4ad5a938abce273
Recep Omer Zorlu <recepomerzorlu@gmail.com>
2022-05-12 12:24:27 +0200

    node cron module added. Pushin it to test it on server if it will work on the server as well.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const cron = require('node-cron');
 
 // In the future can be get as a parameter from the command line
 const port = 3000;
@@ -6,3 +7,5 @@ const port = 3000;
 app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+cron.schedule('* * * * *', () => {console.log("Task is running every minute " + new Date())});
